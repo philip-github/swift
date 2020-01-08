df8c7bdbecef88abc344e2b916add09dbdb2ad2a
Philip Altwal <41710505+philip-github@users.noreply.github.com>
2020-01-08 11:40:01 -0600

    Add files via upload

---
+++ SwiftUIAppleTutorial-1/SwiftUIAppleTutorial-1/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  SwiftUIAppleTutorial-1
+//
+//  Created by Philip Twal on 1/7/20.
+//  Copyright © 2020 Philip Twal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView().edgesIgnoringSafeArea(.top)
+                .frame(height : 300)
+            
+            CircleImage().offset(y: -130).padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park").font(.subheadline)
+                    Spacer()
+                Text("California").font(.subheadline)
+                }
+            }
+            .padding()
+            
+            
+            Spacer()
+        }
+    
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
